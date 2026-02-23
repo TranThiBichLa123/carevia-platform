a9f90b620ac826aaa5414cbb46ff7c4b8d8ecac3
TranThiBichLa123 <23520827@gm.uit.edu.vn>
2026-02-23 15:18:57 +0700

    Add full frontend and backend

---
+++ backend/src/main/java/com/carevia/CareviaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carevia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareviaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareviaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/carevia/CareviaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carevia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareviaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
